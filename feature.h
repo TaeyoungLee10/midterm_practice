56d88aa8264065d9e16a5d5ac2ec17a4005aef8b
taeyoung <taeyounglee19@gmail.com>
2020-04-21 01:11:09 +0900

    feature.h: add, read, list, update func prototype added

--- feature.h
+++ feature.h
@@ -1,1 +1,13 @@
-#include "feature.h"
+//feature.h
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+	char name[20];
+	int score;
+}SCORE;
+
+int add_score();
+int read_score(SCORE p);
+int list_score(SCORE p[], int count);
+int update_score();
